5364aeb907e3e552bd93efc1d4876f40a0e1e74f
alexandrerosa <alexandre.rosa.56@gmail.com>
2024-05-14 17:28:30 +0200

    Fix and simplifications

--- main.go
+++ main.go
@@ -2,53 +2,35 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
-	"regexp"
 	"strings"
 
 	"github.com/fatih/color"
 )
 
+// go run maing.go -l "urls_file_path" -o "output_file_path" -u true -r "url_to_filter"
+
 func main() {
 	logo := `
-░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░       ░▒▓███████▓▒░▒▓████████▓▒░▒▓█▓▒░▒▓█▓▒░   ░▒▓████████▓▒░▒▓████████▓▒░▒▓███████▓▒░  
-░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░       ░▒▓██████▓▒░░▒▓██████▓▒░ ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓██████▓▒░ ░▒▓███████▓▒░  
-░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ 
-░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ 
- ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓███████▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░   ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░ 
-																															  
-																															 `
+	░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░       ░▒▓███████▓▒░▒▓████████▓▒░▒▓█▓▒░▒▓█▓▒░   ░▒▓████████▓▒░▒▓████████▓▒░▒▓███████▓▒░  
+	░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ 
+	░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ 
+	░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░       ░▒▓██████▓▒░░▒▓██████▓▒░ ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓██████▓▒░ ░▒▓███████▓▒░  
+	░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ 
+	░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░ 
+	 ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓███████▓▒░░▒▓█▓▒░      ░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░   ░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+																																  
+																																 `
 	fmt.Println(logo)
-
 	fmt.Printf("[v%v]\n\n", color.BlueString("0.1.0"))
 
-	var inputFile = flag.String("l", "", "Input file")
-	var outputFile = flag.String("o", "", "Output file")
-	var domainsIncludeFilter = flag.String("md", "", "Domain to include")
-	var isUnique = flag.Bool("u", true, "Set if output unique URLs")
-	var isDebug = flag.Bool("debug", false, "Set if output debug")
-	var regexFilter = flag.String("r", "", "Regex filter for URLs")
-	var isJson = flag.Bool("json", false, "Output format in json")
-
-	flag.Parse()
+	inputFile, outputFile, isUnique, domainsIncludeFilter := parseFlags()
 
 	if *inputFile == "" {
-		fmt.Println("Input file is required")
+		fmt.Println("Input file is required") //TODO mettre en rouge
 		return
-	}
-
-	var regex *regexp.Regexp
-	var err error
-	if *regexFilter != "" {
-		regex, err = regexp.Compile(*regexFilter)
-		if err != nil {
-			fmt.Printf("\n%v %v\n", color.RedString("Number of duplicate URLs found : "), err)
-		}
 	}
 
 	file, err := os.Open(*inputFile)
@@ -58,32 +40,26 @@ func main() {
 	}
 	defer file.Close()
 
-	urlMap := make(map[string]bool)
+	urlsFiltered := []string{} //appeler la méthode de filtre
 	scanner := bufio.NewScanner(file)
 	duplicateCount := 0
 
+	// TODO func Filter et ajouter dans tools
 	for scanner.Scan() {
 		url := strings.TrimSpace(scanner.Text())
 
-		// Apply domain filter if provided
 		if *domainsIncludeFilter != "" && !strings.Contains(url, *domainsIncludeFilter) {
 			continue
 		}
 
-		// Apply regex filter if provided
-		if regex != nil && !regex.MatchString(url) {
-			continue
-		}
-
-		// Apply regex filter if provided
 		if *isUnique {
-			if _, exists := urlMap[url]; exists {
-				duplicateCount++
+			if !isUrlInArray(urlsFiltered, url) {
+				urlsFiltered = append(urlsFiltered, url)
 			} else {
-				urlMap[url] = true
+				duplicateCount += 1
 			}
 		} else {
-			urlMap[url] = true
+			urlsFiltered = append(urlsFiltered, url)
 		}
 	}
 
@@ -92,12 +68,7 @@ func main() {
 		return
 	}
 
-	// Prepare the filtered URLs for output
-	var output []string
-	for url := range urlMap {
-		output = append(output, url)
-	}
-
+	// TODO func WriteOuput et ajouter dans tools
 	// Write the filtered URLs to the output file or print them to the consol
 	if *outputFile != "" {
 		outFile, err := os.Create(*outputFile)
@@ -108,44 +79,39 @@ func main() {
 		defer outFile.Close()
 
 		writer := bufio.NewWriter(outFile)
-		if *isJson {
-			jsonData, err := json.MarshalIndent(output, "", "  ")
+
+		for _, url := range urlsFiltered {
+			_, err := writer.WriteString(url + "\n")
 			if err != nil {
-				fmt.Printf("Error marshaling JSON: %v\n", err)
+				fmt.Printf("Error writing to output file: %v\n", err)
 				return
-			}
-			_, err = writer.Write(jsonData)
-			if err != nil {
-				fmt.Printf("Error writing JSON to output file: %v\n", err)
-				return
-			}
-		} else {
-			for _, url := range output {
-				_, err := writer.WriteString(url + "\n")
-				if err != nil {
-					fmt.Printf("Error writing to output file: %v\n", err)
-					return
-				}
 			}
 		}
 		writer.Flush()
 	} else {
-		if *isJson {
-			jsonData, err := json.MarshalIndent(output, "", "  ")
-			if err != nil {
-				fmt.Printf("Error marshaling JSON: %v\n", err)
-				return
-			}
-			fmt.Println(string(jsonData))
-		} else {
-			for _, url := range output {
-				fmt.Println(url)
-			}
+		for _, url := range urlsFiltered {
+			fmt.Println(url)
 		}
 	}
 
-	if *isDebug {
-		fmt.Printf("\n%v %v\n", "Number of duplicate URLs found : ", duplicateCount)
-	}
+	fmt.Printf("\n%v %v\n", "Number of duplicate URLs found : ", duplicateCount)
+}
 
+func parseFlags() (*string, *string, *bool, *string) {
+	inputFile := flag.String("l", "", "Input file")
+	outputFile := flag.String("o", "", "Output file")
+	isUnique := flag.Bool("u", true, "Set if output unique URLs")
+	domainsIncludeFilter := flag.String("r", "", "Regex filter for URLs")
+
+	flag.Parse()
+	return inputFile, outputFile, isUnique, domainsIncludeFilter
+}
+
+func isUrlInArray(arr []string, url string) bool {
+	for _, v := range arr {
+		if v == url {
+			return true
+		}
+	}
+	return false
 }
